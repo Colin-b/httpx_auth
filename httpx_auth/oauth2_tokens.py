d1fc08d8b391890bc1972d4458f03c5ac6b47411
Maxim Claeys <maxim.claeys@robovision.eu>
2022-01-28 14:18:45 +0100

    Add async variant

--- httpx_auth/oauth2_tokens.py
+++ httpx_auth/oauth2_tokens.py
@@ -1,3 +1,4 @@
+import asyncio
 import base64
 import json
 import os
@@ -46,7 +47,9 @@ class TokenMemoryCache:
     def __init__(self):
         self.tokens = {}
         self.forbid_concurrent_cache_access = threading.Lock()
+        self.forbid_concurrent_cache_access_async = asyncio.Lock()
         self.forbid_concurrent_missing_token_function_call = threading.Lock()
+        self.forbid_concurrent_missing_token_function_call_async = asyncio.Lock()
 
     def _add_bearer_token(self, key: str, token: str):
         """
@@ -141,6 +144,68 @@ class TokenMemoryCache:
                         f"Using a token received on another key than expected. Expecting {key} but was {state}."
                     )
             with self.forbid_concurrent_cache_access:
+                if state in self.tokens:
+                    bearer, expiry = self.tokens[state]
+                    logger.debug(
+                        f"Using newly received authentication, expiring on {datetime.datetime.utcfromtimestamp(expiry)} (UTC)."
+                    )
+                    return bearer
+
+        logger.debug(
+            f"User was not authenticated: key {key} cannot be found in {self.tokens}."
+        )
+        raise AuthenticationFailed()
+
+    async def get_token_async(
+        self,
+        key: str,
+        *,
+        early_expiry: float = 30.0,
+        on_missing_token=None,
+        **on_missing_token_kwargs,
+    ) -> str:
+        """
+        Return the bearer token.
+        :param key: key identifier of the token
+        :param early_expiry: As the time between the token extraction from cache and the token reception on server side
+        might not higher than one second, on slow networks, token might be expired when received by the actual server,
+        even if still valid when fetched.
+        This is the number of seconds to subtract to the actual token expiry. Token will be considered as
+        expired 30 seconds before real expiry by default.
+        :param on_missing_token: function to call when token is expired or missing (returning token and expiry tuple)
+        :param on_missing_token_kwargs: arguments of the function (key-value arguments)
+        :return: the token
+        :raise AuthenticationFailed: in case token cannot be retrieved.
+        """
+        logger.debug(f'Retrieving token with "{key}" key.')
+        with self.forbid_concurrent_cache_access_async:
+            self._load_tokens()
+            if key in self.tokens:
+                bearer, expiry = self.tokens[key]
+                if _is_expired(expiry, early_expiry):
+                    logger.debug(f'Authentication token with "{key}" key is expired.')
+                    del self.tokens[key]
+                else:
+                    logger.debug(
+                        f"Using already received authentication, will expire on {datetime.datetime.utcfromtimestamp(expiry)} (UTC)."
+                    )
+                    return bearer
+
+        logger.debug("Token cannot be found in cache.")
+        if on_missing_token is not None:
+            with self.forbid_concurrent_missing_token_function_call_async:
+                new_token = await on_missing_token(**on_missing_token_kwargs)
+                if len(new_token) == 2:  # Bearer token
+                    state, token = new_token
+                    self._add_bearer_token(state, token)
+                else:  # Access Token
+                    state, token, expires_in = new_token
+                    self._add_access_token(state, token, expires_in)
+                if key != state:
+                    logger.warning(
+                        f"Using a token received on another key than expected. Expecting {key} but was {state}."
+                    )
+            with self.forbid_concurrent_cache_access_async:
                 if state in self.tokens:
                     bearer, expiry = self.tokens[state]
                     logger.debug(
