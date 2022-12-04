339a305a011473d07e26938db2728c15cac871e2
Colin-b <Colin-b@users.noreply.github.com>
2022-12-04 23:35:33 +0100

    Draft to handle more content-type than JSON when receiving tokens

--- httpx_auth/authentication.py
+++ httpx_auth/authentication.py
@@ -59,6 +59,17 @@ def _get_query_parameter(url: str, param_name: str) -> Optional[str]:
     return all_values[0] if all_values else None
 
 
+def _content_from_response(response: httpx.Response) -> dict:
+    content_type = response.headers.get("content-type")
+    if content_type == "text/html; charset=utf-8":
+        return {
+            key_values[0]: key_values[1]
+            for key_value in response.text.split("&")
+            if (key_values := key_value.split("=")) and len(key_values) == 2
+        }
+    return response.json()
+
+
 def request_new_grant_with_post(
     url: str, data, grant_name: str, client: httpx.Client
 ) -> (str, int):
@@ -68,7 +79,7 @@ def request_new_grant_with_post(
         # As described in https://tools.ietf.org/html/rfc6749#section-5.2
         raise InvalidGrantRequest(response)
 
-    content = response.json()
+    content = _content_from_response(response)
     token = content.get(grant_name)
     if not token:
         raise GrantNotProvided(grant_name, content)
