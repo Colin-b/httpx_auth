12aef137065a42dfc8b678b59ccfbbc7c080a512
Colin-b <Colin-b@users.noreply.github.com>
2024-01-26 17:10:31 +0100

    Ensure HTTP method is not modified by auth and use httpx Client as this is the recommended way

--- tests/api_key/test_api_key_sync.py
+++ tests/api_key/test_api_key_sync.py
@@ -3,32 +3,47 @@ import httpx
 
 
 import httpx_auth
-from tests.auth_helper import get_header
 
 
 def test_header_api_key_is_sent_in_x_api_key_by_default(httpx_mock: HTTPXMock):
     auth = httpx_auth.HeaderApiKey("my_provided_api_key")
-    assert get_header(httpx_mock, auth).get("X-Api-Key") == "my_provided_api_key"
+
+    httpx_mock.add_response(
+        url="https://authorized_only",
+        method="GET",
+        match_headers={"X-API-KEY": "my_provided_api_key"},
+    )
+    with httpx.Client() as client:
+        client.get("https://authorized_only", auth=auth)
 
 
 def test_query_api_key_is_sent_in_api_key_by_default(httpx_mock: HTTPXMock):
     auth = httpx_auth.QueryApiKey("my_provided_api_key")
-    # Mock a dummy response
-    httpx_mock.add_response(url="https://authorized_only?api_key=my_provided_api_key")
-    # Send a request to this dummy URL with authentication
-    httpx.get("https://authorized_only", auth=auth)
+
+    httpx_mock.add_response(
+        url="https://authorized_only?api_key=my_provided_api_key", method="GET"
+    )
+    with httpx.Client() as client:
+        client.get("https://authorized_only", auth=auth)
 
 
 def test_header_api_key_can_be_sent_in_a_custom_field_name(httpx_mock: HTTPXMock):
     auth = httpx_auth.HeaderApiKey("my_provided_api_key", "X-API-HEADER-KEY")
-    assert get_header(httpx_mock, auth).get("X-Api-Header-Key") == "my_provided_api_key"
+
+    httpx_mock.add_response(
+        url="https://authorized_only",
+        method="GET",
+        match_headers={"X-API-HEADER-KEY": "my_provided_api_key"},
+    )
+    with httpx.Client() as client:
+        client.get("https://authorized_only", auth=auth)
 
 
 def test_query_api_key_can_be_sent_in_a_custom_field_name(httpx_mock: HTTPXMock):
     auth = httpx_auth.QueryApiKey("my_provided_api_key", "X-API-QUERY-KEY")
-    # Mock a dummy response
+
     httpx_mock.add_response(
-        url="https://authorized_only?X-API-QUERY-KEY=my_provided_api_key"
+        url="https://authorized_only?X-API-QUERY-KEY=my_provided_api_key", method="GET"
     )
-    # Send a request to this dummy URL with authentication
-    httpx.get("https://authorized_only", auth=auth)
+    with httpx.Client() as client:
+        client.get("https://authorized_only", auth=auth)
