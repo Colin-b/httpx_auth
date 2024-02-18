8a45712f769602f51418436a791e7ca8d0e4dde7
Colin-b <Colin-b@users.noreply.github.com>
2024-02-18 14:05:55 +0100

    Use renamed parameter

--- tests/oauth2/authorization_code/test_oauth2_authorization_code_async.py
+++ tests/oauth2/authorization_code/test_oauth2_authorization_code_async.py
@@ -62,7 +62,7 @@ async def test_oauth2_authorization_code_flow_uses_custom_success(
     tab = browser_mock.add_response(
         opened_url="https://provide_code?response_type=code&state=ce9c755b41b5e3c5b64c70598715d5de271023a53f39a67a70215d265d11d2bfb6ef6e9c701701e998e69cbdbf2cee29fd51d2a950aa05f59a20cf4a646099d5&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2F",
         reply_url="http://localhost:5000#code=SplxlOBeZQQYbYS6WxSbIA&state=ce9c755b41b5e3c5b64c70598715d5de271023a53f39a67a70215d265d11d2bfb6ef6e9c701701e998e69cbdbf2cee29fd51d2a950aa05f59a20cf4a646099d5",
-        success_html="<body><div>SUCCESS: {display_time}</div></body>",
+        displayed_html="<body><div>SUCCESS: {display_time}</div></body>",
     )
     httpx_mock.add_response(
         method="POST",
